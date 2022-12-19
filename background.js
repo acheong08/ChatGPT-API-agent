0d9a1bfa0d56e8bcf17137205c2d9ff49b9d204c
Antonio Cheong <acheong@student.dalat.org>
2022-12-19 16:54:43 +0800

    Add container functionality

--- background.js
+++
@@ -1,6 +0,0 @@
-// Add an event listener for the browser action icon click event
-browser.browserAction.onClicked.addListener(() => {
-    // Create a new tab
-    browser.tabs.create({url: "https://chat.openai.com"});
-  });
-  
---
+++ browser_handlers.js
@@ -0,0 +1,32 @@
+// Add an event listener for the browser action icon click event
+browser.browserAction.onClicked.addListener(() => {
+  // Create a new tab
+  new_tab();
+});
+
+browser.tabs.onRemoved.addListener(() => {
+  // Get container ID from local storage
+  browser.storage.local.get("containerId").then((result) => {
+    // Delete the container
+    browser.contextualIdentities.remove(result.containerId);
+  });
+});
+
+function new_tab() {
+  // Create a new container with tabID
+  browser.contextualIdentities
+    .create({
+      name: "OpenAI (tmp)",
+      color: "blue",
+      icon: "circle",
+    })
+    .then((container) => {
+      // Create a new tab in the container with the tabID and URL
+      browser.tabs.create({
+        cookieStoreId: container.cookieStoreId,
+        url: "https://chat.openai.com",
+      });
+      // Store container ID in local storage
+      browser.storage.local.set({ containerId: container.cookieStoreId });
+    });
+}
