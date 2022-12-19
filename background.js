2e50160682c60e2cc3d164023d2be8a95556c0e8
Antonio Cheong <acheong@student.dalat.org>
2022-12-19 18:30:04 +0800

    Inject script

---
+++ background.js
@@ -0,0 +1,39 @@
+// Add an event listener for the browser action icon click event
+browser.browserAction.onClicked.addListener(() => {
+  // Create a new tab
+  new_tab();
+});
+
+browser.tabs.onRemoved.addListener(() => {
+  // Get container ID from local storage
+  browser.storage.local.get("containerId").then((result) => {
+    // Delete the container
+    browser.contextualIdentities.remove(result.containerId);
+  });
+});
+
+function new_tab() {
+  // Create a new container with tabID
+  browser.contextualIdentities
+    .create({
+      name: "OpenAI (tmp)",
+      color: "blue",
+      icon: "circle",
+    })
+    .then((container) => {
+      // Create a new tab in the container with the tabID and URL
+      browser.tabs
+        .create({
+          cookieStoreId: container.cookieStoreId,
+          url: "https://chat.openai.com",
+        })
+        .then((tab) => {
+          // Inject the content script into the tab
+          browser.tabs.executeScript(tab.id, {
+            file: "content.js",
+          });
+        });
+      // Store container ID in local storage
+      browser.storage.local.set({ containerId: container.cookieStoreId });
+    });
+}
