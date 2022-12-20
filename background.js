8d1814121c5d26b8c9da516b7a37294a9844e3c6
Antonio Cheong <acheong@student.dalat.org>
2022-12-20 18:45:13 +0800

    Fix websocket bugs

--- background.js
+++ background.js
@@ -12,15 +12,6 @@ browser.tabs.onRemoved.addListener(() => {
   });
 });
 
-browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (changeInfo.status === 'complete') {
-    browser.tabs.executeScript(tabId, {
-      file: 'src/content.js',
-    });
-  }
-});
-
-
 function new_tab() {
   // Create a new container with tabID
   browser.contextualIdentities
@@ -31,11 +22,10 @@ function new_tab() {
     })
     .then((container) => {
       // Create a new tab in the container with the tabID and URL
-      browser.tabs
-        .create({
-          cookieStoreId: container.cookieStoreId,
-          url: "https://chat.openai.com",
-        })
+      browser.tabs.create({
+        cookieStoreId: container.cookieStoreId,
+        url: "https://chat.openai.com",
+      });
       // Store container ID in local storage
       browser.storage.local.set({ containerId: container.cookieStoreId });
     });
--- src/content.js
+++ src/content.js
@@ -4,15 +4,31 @@ browser.storage.local.get("endpoint").then((result) => {
   if (result.endpoint) {
     // Set the endpoint
     endpoint = result.endpoint;
+  } else {
+    // Set the endpoint to the default value
+    endpoint = "localhost:8080";
+  }
+  // Make GET websocket request to endpoint
+  let ws_route = "ws://" + endpoint + "/client/register";
+  let ws = new WebSocket(ws_route);
+  // Wait for websocket to open
+  ws.onopen = function () {
+    // Wait for message from server
+    ws.onmessage = function (event) {
+      // Log the message
+      console.log(event.data);
+      // Parse data as JSON
+      let data = JSON.parse(event.data);
+      // If message is "Connection id" then set the connection id
+      if (data.message == "Connection id") {
+        connection_id = data.id;
+        // Return id to websocket
+        let message = {
+          "message": "Connection id",
+          "id": connection_id,
+        }
+        ws.send(JSON.stringify(message));
+      }
+    }
   }
 });
-// Make GET websocket request to endpoint
-let ws_route = "ws://" + endpoint + "/client/register";
-let ws = new WebSocket(ws+route);
-// Wait for websocket to open
-ws.onopen = function() {
-    // Wait for websocket to receive message
-    ws.onmessage = function (event) {
-        // Do stuff with message
-    }
-}
