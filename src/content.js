45980036ea7ff4d11e5af3641fc6391702d20fe7
Antonio Cheong <acheong@student.dalat.org>
2022-12-20 23:10:54 +0800

    ping

--- src/content.js
+++ src/content.js
@@ -1,34 +1,44 @@
-// Get endpoint from storage
 browser.storage.local.get("endpoint").then((result) => {
-  // If the endpoint string exists, set the value of the form's endpoint input
   if (result.endpoint) {
-    // Set the endpoint
     endpoint = result.endpoint;
   } else {
-    // Set the endpoint to the default value
     endpoint = "localhost:8080";
   }
-  // Make GET websocket request to endpoint
   let ws_route = "ws://" + endpoint + "/client/register";
   let ws = new WebSocket(ws_route);
-  // Wait for websocket to open
   ws.onopen = function () {
-    // Wait for message from server
     ws.onmessage = function (event) {
-      // Log the message
       console.log(event.data);
-      // Parse data as JSON
       let data = JSON.parse(event.data);
-      // If message is "Connection id" then set the connection id
       if (data.message == "Connection id") {
         connection_id = data.id;
-        // Return id to websocket
         let message = {
-          "message": "Connection id",
-          "id": connection_id,
-        }
+          message: "Connection id",
+          id: connection_id,
+          data: "",
+        };
         ws.send(JSON.stringify(message));
+        browser.storage.local.set({ connectionId: connection_id });
+      } else if (data.message == "ping"){
+        let message = {
+          message: "pong",
+          id: data.id,
+          data: "",
+        };
+        ws.send(JSON.stringify(message));
+      } else if (data.message == "ChatGptRequest") {
+        let response = {
+          id: data.id,
+          response_id: "somerandomuuid",
+          conversation_id: "somerandomuuid",
+          content: "Hello world!",
+        };
+        ws.send(JSON.stringify(response));
       }
-    }
-  }
+    };
+  };
+  ws.onclose = function () {
+    console.log("Connection closed");
+    delete connection_id;
+  };
 });
