c39b708561d5d9dc7a27e6accaf11a17aefd9508
Antonio Cheong <acheong@student.dalat.org>
2022-12-23 01:14:59 +0800

    refactor

--- src/login.js
+++ src/login.js
@@ -1,20 +1,26 @@
 /// login.js
 console.log("login.js loaded");
 
-// Get credentials from cookies
-const cookies = document.cookie.split("; ");
-// Get email and password from cookies
-let email = cookies.find((cookie) => cookie.startsWith("email=")).split("=")[1];
-let password = cookies.find((cookie) => cookie.startsWith("password=")).split("=")[1];
-
 // Find the button with the text "Log in"
 let button = document.querySelector(
   ".btn.flex.justify-center.gap-2.btn-primary"
 );
 
+console.log("button: ", button);
+
 if (button && button.innerText === "Log in") {
   button.click();
+} else {
+  console.log("button not found");
 }
+
+// Get credentials from cookies
+const cookies = document.cookie.split("; ");
+// Get email and password from cookies
+let email = cookies.find((cookie) => cookie.startsWith("email=")).split("=")[1];
+let password = cookies
+  .find((cookie) => cookie.startsWith("password="))
+  .split("=")[1];
 
 function setUsername() {
   let input = document.getElementById("username");
@@ -22,7 +28,7 @@ function setUsername() {
     input.value = email;
     console.log("email: ", email);
   } else {
-    setTimeout(setUsername, 100); // try again in 100 milliseconds
+    setTimeout(setUsername, 1000);
   }
 }
 
