0a43358ae06dae40cee071765190a8163eca571c
Antonio Cheong <acheong@student.dalat.org>
2022-12-23 01:03:36 +0800

    Autofill email success

--- src/login.js
+++ src/login.js
@@ -1,5 +1,12 @@
 /// login.js
 console.log("login.js loaded");
+
+// Get credentials from cookies
+const cookies = document.cookie.split("; ");
+// Get email and password from cookies
+let email = cookies.find((cookie) => cookie.startsWith("email=")).split("=")[1];
+let password = cookies.find((cookie) => cookie.startsWith("password=")).split("=")[1];
+
 // Find the button with the text "Log in"
 let button = document.querySelector(
   ".btn.flex.justify-center.gap-2.btn-primary"
@@ -12,7 +19,8 @@ if (button && button.innerText === "Log in") {
 function setUsername() {
   let input = document.getElementById("username");
   if (input) {
-    input.value = "myUsername";
+    input.value = email;
+    console.log("email: ", email);
   } else {
     setTimeout(setUsername, 100); // try again in 100 milliseconds
   }
